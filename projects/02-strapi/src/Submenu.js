cb0cc3bf0b3f784b76325e4823333d526f6bd9ec
pavanajmera <ajmerapavan20@gmail.com>
2024-09-09 23:50:48 +0530

    submenu logic and css changes

--- projects/02-strapi/src/Submenu.js
+++ projects/02-strapi/src/Submenu.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import { useGlobalContext } from './Context'
+import sublinks from './data';
 
 const Submenu = () => {
+  const {pageId} = useGlobalContext();
+  const currentPage = sublinks.find(item => item.pageId === pageId);
+
   return (
-    <div>
-      Submenu
+    <div className={currentPage? 'submenu show-submenu' : 'submenu'}>
+      <h5>
+        {currentPage?.page}
+      </h5>
+      <div className='submenu-links' style={{gridTemplateColumns:currentPage?.links?.length > 3 ? '1 fr 1fr' : '1fr'}}>
+      {currentPage?.links?.map((link) => {
+        const {id, url, label, icon} = link;
+        return(
+          <a key={id} href={url}>{icon}{label}</a>
+        )
+      })}
+      </div>
     </div>
   )
 }
